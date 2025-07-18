82667be957135e63c6f370e8bb5143009836feb4
6ajmon <6ajmon@gmail.com>
2025-07-19 01:07:12 +0200

    Add alt tall terrain cell

--- source/gridMap/map_generator.gd
+++ source/gridMap/map_generator.gd
@@ -1,14 +1,14 @@
 extends Node3D
 
 @export var grid_map: GridMap
-@export var generatedMapSize = Vector2i(100, 100)
+@export var generatedMapSize = Vector2i(600, 400)
 @export var noise_scale: float = 0.1
-@export var height_threshold: float = 0.3
+@export var height_threshold: float = 0.25
 @export var seed_value: int = 0
 
-# Tile IDs z MeshLibrary
 const TILE_LOW = 0
 const TILE_TALL = 6
+const TILE_TALL_ALT = 2
 const TILE_SLOPE = 3
 const TILE_SLOPE_INNER = 4
 const TILE_SLOPE_OUTER = 5
@@ -30,7 +30,10 @@ func generate_map() -> void:
 			if height < height_threshold:
 				grid_map.set_cell_item(Vector3i(x, 0, y), TILE_LOW)
 			elif height > height_threshold:
-				grid_map.set_cell_item(Vector3i(x, 0, y), TILE_TALL)
+				if randi() % 12 != 0:
+					grid_map.set_cell_item(Vector3i(x, 0, y), TILE_TALL)
+				else:
+					grid_map.set_cell_item(Vector3i(x, 0, y), TILE_TALL_ALT)
 	
 
 			
