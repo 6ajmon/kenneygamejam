13d712e0bf338feb3b4a44e70561205189a7acf8
6ajmon <6ajmon@gmail.com>
2025-07-19 23:20:16 +0200

    Add loading screen and animation to vehicle on ui

---
+++ source/ui/loading_screen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var target_scene_path: String = "res://source/gridMap/grid_map.tscn"
+
+var load_state = null
+
+func _ready():
+	load_state = ResourceLoader.load_threaded_request(target_scene_path)
+
+func _process(_delta):
+	if load_state == null:
+		return
+
+	var status = ResourceLoader.load_threaded_get_status(target_scene_path)
+	if status == ResourceLoader.THREAD_LOAD_LOADED:
+		var scene_res = ResourceLoader.load_threaded_get(target_scene_path)
+		var new_scene = scene_res.instantiate()
+
+		get_tree().root.add_child(new_scene)
+		get_tree().current_scene.queue_free()
+		get_tree().current_scene = new_scene
--- source/ui/starting_screen.gd
+++ source/ui/starting_screen.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var first_level_scene: PackedScene = preload("res://source/gridMap/grid_map.tscn")
+@onready var loading_screen: PackedScene = preload("res://source/ui/loading_screen.tscn")
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_packed(first_level_scene)
+	get_tree().change_scene_to_packed(loading_screen)
