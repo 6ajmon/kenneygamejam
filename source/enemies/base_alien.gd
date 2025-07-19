60cc9cfc9d8c846797809e719dfcddcf1c6bf7aa
6ajmon <6ajmon@gmail.com>
2025-07-19 14:15:09 +0200

    Add simple code for alien to run away

--- source/enemies/base_alien.gd
+++ source/enemies/base_alien.gd
@@ -1,8 +1,25 @@
 extends CharacterBody3D
 
-var gravity = 5
+@export var player_vehicle: PlayerVehicle
+@export var speed: float = 3.0
+@export var player_detection_range: float = 6.0
+
+var gravity = 20
+
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y -= gravity * delta
 		
-	move_and_slide()
+	run_from_player(delta)
+
+func run_from_player(delta) -> void:
+	if player_vehicle:
+		var direction = global_transform.origin - player_vehicle.global_transform.origin
+		direction.y = 0
+		direction = direction.normalized()
+		
+		velocity = direction * speed
+		velocity.y -= gravity * delta
+		move_and_slide()
+	else:
+		return
