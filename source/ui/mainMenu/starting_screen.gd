075688bbada7f57dfa9c88ae1decef933ec2a9f2
6ajmon <6ajmon@gmail.com>
2025-07-26 12:03:01 +0200

    Add tutorial button to main screen

--- source/ui/mainMenu/starting_screen.gd
+++ source/ui/mainMenu/starting_screen.gd
@@ -11,3 +11,7 @@ func _on_source_code_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_packed(SceneManager.settings_screen)
+
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_packed(SceneManager.tutorial_screen)
--- source/ui/tutorial.gd
+++ source/ui/tutorial.gd
@@ -1,9 +1,1 @@
 extends Control
-
-func _ready() -> void:
-	visible = not GameData.tutorial_closed
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("close_tutorial"):
-		GameData.tutorial_closed = not GameData.tutorial_closed
-		visible = not GameData.tutorial_closed
