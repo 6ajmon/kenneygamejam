309f924c33541af3b56fc184dea001d0350392b1
6ajmon <6ajmon@gmail.com>
2025-08-11 07:21:47 +0200

    Update starting_screen.gd

--- source/ui/mainMenu/starting_screen.gd
+++ source/ui/mainMenu/starting_screen.gd
@@ -4,4 +4,4 @@ func _ready():
 	AudioManager.emit_signal("menu_music")
 
 func _on_source_button_pressed() -> void:
-	OS.shell_open("https://github.com/6ajmon/kenneygamejam")
+	OS.shell_open("https://github.com/6ajmon/Deadline")
